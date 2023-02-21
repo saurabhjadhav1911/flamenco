daaadbf2fda7ec0ed53564c4d4ea4ad2bb01aea7
Sybren A. Stüvel <sybren@blender.org>
2023-02-21 11:35:45 +0100

    Webapp: give the Flamenco logo a bit of a black outline
    A black outline around the white shape makes it visible in light themed
    browser windows.

---
+++ web/app/public/mkico.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+convert flamenco.png -bordercolor '#00000000' -border 0 \
+          \( -clone 0 -resize 16x16 \) \
+          \( -clone 0 -resize 32x32 \) \
+          \( -clone 0 -resize 48x48 \) \
+          \( -clone 0 -resize 64x64 \) \
+          -delete 0 -colors 256 favicon.ico
